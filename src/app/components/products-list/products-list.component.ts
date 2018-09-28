18f15718cdd8ee0461e5f32b3e53748b98f7dfb3
bpawluczuk <b.pawluczuk@companion.pl>
2018-09-28 18:45:01 +0200

    commit

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductsService} from '../../services/products/products.service';
 import {Product} from '../../Types/product';
+import {interval, of} from 'rxjs';
+import {catchError, map, retry} from 'rxjs/operators';
+import {ajax} from 'rxjs/ajax';
 
 @Component({
   selector: 'app-products-list',
@@ -13,19 +16,44 @@ export class ProductsListComponent implements OnInit {
   }
 
   products: Product[];
+  test: string;
 
   ngOnInit() {
     this.getProductsList();
   }
 
   getProductsList(): void {
-    this.productsService.getProductsList()
-      .subscribe(products => {
-        if (products['status'] === 'ok') {
-          this.products = products['data'];
-        } else {
-          this.products = null;
+
+    const apiData = this.productsService.getProductsList().pipe(
+      retry(3),
+      map(products => {
+        if (!products['status']) {
+          throw new Error('Value expected!');
         }
-      });
+        this.products = products['data'];
+        return products;
+      }),
+      catchError(err => of([]))
+    );
+
+    apiData.subscribe({
+      next(products) {
+        this.products = products['data'];
+        console.log('data: ', products);
+      },
+      error(err) {
+        console.log('errors already caught... will not run');
+      }
+    });
+
+    // this.productsService.getProductsList()
+    //   .subscribe(products => {
+    //     if (products['status'] === 'ok') {
+    //       this.products = products['data'];
+    //     } else {
+    //       this.products = null;
+    //     }
+    //   });
+
   }
 }
